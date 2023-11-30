a9244776295a6fb058cca11b88d8dec3c4e7c9bb
Danayt09 <danaytaman@gmail.com>
2023-11-29 22:28:48 -0500

    xgboost

---
+++ tz_wells_app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import joblib
+
+st.title('Tanzanian Water Well')
+st.header('Functionality Prediction')
+
+st.sidebar.title('Contact Information')
+st.sidebar.header('Danayt Aman')
+st.sidebar.subheader('Data Analyst/Data Scientist')
+
